24f5e1c17c49a91b672241761283d8e044d62585
ElenaFrank <alyonazholob3@gmail.com>
2022-10-17 21:40:08 +0700

    2

---
+++ src/components/navBar.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const NavBar = () => {
+    return ( 
+
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/login">Login</a></li>
+            <li><a href="/pasts">Pasts</a></li>
+            <li><a href="/ashboard">Dashboard</a></li>
+        </ul>
+     )
+}
+ 
+export default NavBar
---
+++ src/components/pasts.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Pasts = () => {
+    return ( <h1>Pasts</h1> )
+}
+ 
+export default Pasts
