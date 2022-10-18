b06f6f05c7ef322310695bb524a223b01efbdd8e
ElenaFrank <alyonazholob3@gmail.com>
2022-10-18 20:19:27 +0700

    add Switch and Link

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,23 @@
 import NavBar from "../components/navBar"
-import {Route} from "react-router-dom"
+import {Route, Switch} from "react-router-dom"
 import Dashboard from "../components/dashboard"
+import Home from "../components/home"
+import Login from "../components/login"
+import Pasts from "../components/pasts"
 
 function App() {
   return (
     <div>
       <NavBar/>
       <h1>App</h1>
-      <Route path="/dashboard" component = {Dashboard} />
+      <Switch>
+
+        <Route path="/dashboard" component = {Dashboard} />
+        <Route path="/login" component = {Login} />
+        <Route path="/pasts" component = {Pasts} />
+        <Route path="/" component = {Home} />
+
+      </Switch>
     </div>  
   )
 }
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,13 +1,22 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
     return ( 
 
         <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/login">Login</a></li>
-            <li><a href="/pasts">Pasts</a></li>
-            <li><a href="/dashboard">Dashboard</a></li>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/login">Login</Link>
+            </li>
+            <li>
+                <Link to="/pasts">Pasts</Link>
+            </li>
+            <li>
+                <Link to="/dashboard">Dashboard</Link>
+            </li>
         </ul>
      )
 }
